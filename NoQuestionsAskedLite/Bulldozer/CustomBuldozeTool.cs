4fc8e52b9b7da423a8246687e596ef0b9540e3f1
Aiden Van Dyke <aidenavandyke@gmail.com>
2021-04-27 14:31:23 -0600

    Add project files.

---
+++ NoQuestionsAskedLite/Bulldozer/CustomBuldozeTool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace NoQuestionsAskedLite
+{
+    public class CustomBuldozeTool : DefaultTool
+    {
+        public IEnumerator TryDeleteBuilding(ushort building)
+        {
+            // The original TryDeleteBuilding invocation has been redirected to this method using the Detour library. This method should return
+            // an IEnumerator implementation for doing the actual work.
+            return new TryDeleteBuildingEnumerator(building);
+        }
+    }
+}
